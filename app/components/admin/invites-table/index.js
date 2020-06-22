198fadbeb8312fa9083f89ca44aae25888cb70af
Kalob Porter <kalob@kalob.net>
2020-06-21 22:03:39 -0400

    Placeholder for invites table

--- app/components/admin/invites-table/index.js
+++ app/components/admin/invites-table/index.js
@@ -1,6 +1,9 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class InvitesTable extends Component {
+  @service('store') store;
+
   get invites() {
     return this.store.peekAll('invites');
   }
