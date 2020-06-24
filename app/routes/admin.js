c8edd3cd2931cad0188f11ed7c04dfd9ccac627d
Kalob Porter <kalob@kalob.net>
2020-06-23 22:56:11 -0400

    Fix login page redirect

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,10 +4,13 @@ import { inject as service } from '@ember/service';
 export default class AdminRoute extends Route {
   @service('session') session
 
-  async beforeModel() {
-    await this.session.fetch();
+  beforeModel() {
+    return this.session.fetch().catch(() => {});
+  }
 
+  model() {
     if (!this.session.get('isAuthenticated')) {
+      this.session.close();
       this.transitionTo('login');
     }
   }
